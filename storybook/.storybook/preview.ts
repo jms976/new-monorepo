8ca99cbba4a845aff92e4b4dc75c89ac696c8acb
myungsujang <myungsujang89@gmail.com>
2025-05-16 00:39:37 +0900

    fix: 테마변경

--- storybook/.storybook/preview.ts
+++ storybook/.storybook/preview.ts
@@ -2,6 +2,7 @@ import '@common/ui/src/styles/globals.css';
 import '@common/ui/src/styles/fonts/defaultFonts.css';
 
 import type { Preview } from '@storybook/react';
+import { palette } from '@common/tokens';
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +11,13 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'light', value: palette.juiGrey.a100 }, // 기본 흰색 배경
+        { name: 'dark', value: palette.juiBackground.default }, // 어두운 테마용
+      ],
     },
   },
 };
