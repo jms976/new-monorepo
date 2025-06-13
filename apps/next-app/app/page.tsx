a7f8c9e735d841d7b9356000531125ab7c42fd61
myungsujang <myungsujang89@gmail.com>
2025-06-13 22:05:19 +0900

    fix: lint test

--- apps/next-app/app/page.tsx
+++ apps/next-app/app/page.tsx
@@ -9,8 +9,6 @@ export default function Page() {
 
   console.log('first');
   console.log('first');
-  console.log('first');
-  console.log('first');
 
   return (
     <div className="flex items-center justify-center min-h-svh">
