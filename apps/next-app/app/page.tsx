73afd312f9d7a032f6374a55451378ff91334b50
myungsujang <myungsujang89@gmail.com>
2025-06-13 21:59:45 +0900

    fix: lint test

--- apps/next-app/app/page.tsx
+++ apps/next-app/app/page.tsx
@@ -7,12 +7,6 @@ import CustomDialog from './components/CustomDialog';
 export default function Page() {
   const propsTest = 'props 테스트';
   console.log('first');
-  console.log('first');
-  console.log('first');
-  console.log('first');
-  console.log('first');
-  console.log('first');
-  console.log('first');
 
   return (
     <div className="flex items-center justify-center min-h-svh">
